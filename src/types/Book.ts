arminfro/alfatraining_02-21

A9, add BookEdit, BookCreate

src/types/Book.ts
@@ -2,7 +2,7 @@ export default interface Book extends BookBase {
   published: Date;
 }
 
-interface BookRaw extends BookBase {
+export interface BookRaw extends BookBase {
   published: string;
 }
 

markYmarkgo/alfatraining_08-21

src/types/Book.ts
@@ -2,7 +2,7 @@ export interface Book extends BookWithoutDate {
   published: Date;
 }
 
-interface BookWithDateString extends BookWithoutDate {
+export interface BookWithDateString extends BookWithoutDate {
   published: string
 }
 